ee3e1d7a9eea548161f6514c18f7f808f42c33e9
adityadesign <68054763+adityadesign@users.noreply.github.com>
2023-06-23 18:46:24 +0530

    Refractoring the code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   const [dataArr, setDataArr] = useState([])
   const [filteredArr, setFilteredArr] = useState([])
+  const [flag, setFlag] = useState(false)
 
   //Fetching Data from API
   useEffect(() => {
@@ -21,11 +22,19 @@ function App() {
   //For searching the first_name from the Data
   const handleChange = (e) => {
     const searchName = e.target.value.toLowerCase()
+
     if(searchName){
       const filteredItems = dataArr.filter(item => {
         return item.first_name.toLowerCase().includes(searchName)
       })
       setFilteredArr(filteredItems)
+
+      //If the searched name is not incorrect, then the flag is set to true to display 'Result Not Found' page.
+      if(filteredItems.length===0){
+        setFlag(true)
+      } else if(filteredItems.length>0){
+        setFlag(false)
+      }
     } else {
       setFilteredArr(dataArr)
     }
@@ -38,9 +47,9 @@ function App() {
         <input className="searchBox" id="name" type="text" onChange={e => handleChange(e)}/>
       </div>
 
-      <DisplayLists filteredArr={filteredArr}/>
+      {dataArr.length===0 ? <div className="loading">Loading...</div> : <DisplayLists filteredArr={filteredArr}/>}
 
-      {filteredArr.length === 0 && (
+      {flag && (
         <>
           <div className="notFound">Results not found</div>
           <img className="notFoundImg" src="/notFound.svg" alt="" />
